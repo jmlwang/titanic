4808c5d1e8f6a595c96165ce0413b47accb8a95b
jmlwang <jmlwang@berkeley.edu>
2019-01-14 22:01:58 -0800

    functionized age bucketing and prefix breakdowns - tested these models and increased accuracy

--- titanic.R
+++ titanic.R
@@ -1,17 +1,30 @@
 library(plyr)
 library(ggplot2)
 library(caret)
+library(mice)
 
 # load training and test sets
-train = read.csv("~/Desktop/projects/titanic2/train.csv")
-test = read.csv("~/Desktop/projects/titanic2/test.csv")
+# train = read.csv("~/Desktop/projects/titanic2/train.csv")
+# test = read.csv("~/Desktop/projects/titanic2/test.csv")
+train = read.csv("~/Desktop/titanic/train.csv")
+test = read.csv("~/Desktop/titanic/test.csv")
 
 # data summary
+summary(train)
+lapply(train, class)
+
+# how many survived
 survival_count = count(train, vars="Survived")
 survival_count
-
 # proportion of each gender that survived and died
 prop.table(table(train$Sex,train$Survived),1)
+
+# replace empty vals in factor columns with NA
+levels(train$Embarked)[nchar(levels(train$Embarked))==0] = NA
+levels(train$Cabin)[nchar(levels(train$Cabin))==0] = NA
+levels(train$Ticket)[nchar(levels(train$Ticket))==0] = NA
+levels(train$Name)[nchar(levels(train$Name))==0] = NA
+levels(train$Sex)[nchar(levels(train$Sex))==0] = NA
 
 ### initial plots
 ggplot() + geom_bar(aes(x=Survived, fill=Sex), data=train) # by_sex 
@@ -22,19 +35,36 @@ ggplot(data=train,aes(x=Age,fill=as.factor(Survived))) + geom_histogram(position
 
 ## deal w/ missing values
 apply(train,2, function(x) mean(is.na(x)))
+# drop cabin b/c 78% is missing 
+
+# try imputing Age values using mice package
+md.pattern(train)
+
+# imputed_train = mice(train, m=5, method = 'pmm', seed = 500)
+# # using predictive mean matching
+# summary(imputed_train)
 # tried getting rid of rows without Age values. It increased the validation accuracy but it didn't increase the test accuracy.
-median_age = median(train$Age[!is.na(train$Age)])
-train$Age[is.na(train$Age)] = median_age
+median_age <- function(data) {
+  median_age = median(data$Age[!is.na(data$Age)])
+  data$Age[is.na(data$Age)] = median_age
+  return(data)
+}
+train = median_age(train)
+test = median_age(test)
 
 # age by bucket
-# train$Age[which(train$Age > 0 & train$Age < 12)] = "0-11"
-# train$Age[which(train$Age >= 12 & train$Age < 18)] = "12-17"
-# train$Age[which(train$Age >= 18 & train$Age < 25)] = "18-24"
-# train$Age[which(train$Age >= 25 & train$Age < 40)] = "25-39"
-# train$Age[which(train$Age >= 40 & train$Age < 65)] = "40-64"
-# train$Age[which(train$Age >= 65)] = "65+"
-# train$Age[which(is.na(train$Age))] = "25-39"
-# 
+# bucket_ages <- function(data) {
+#   data$Age[which(data$Age > 0 & data$Age < 12)] = "0-11"
+#   data$Age[which(data$Age >= 12 & data$Age < 18)] = "12-17"
+#   data$Age[which(data$Age >= 18 & data$Age < 25)] = "18-24"
+#   data$Age[which(data$Age >= 25 & data$Age < 40)] = "25-39"
+#   data$Age[which(data$Age >= 40 & data$Age < 65)] = "40-64"
+#   data$Age[which(data$Age >= 65)] = "65+"
+#   data$Age[which(is.na(data$Age))] = "25-39"
+#   return(data)
+# }
+# train = bucket_ages(train)
+# test = bucket_ages(test)
 # by_age = ggplot() + geom_bar(aes(x=Age, fill=Survived),data=train)
 
 ### create new features and test for correlation
@@ -50,23 +80,29 @@ train['cabin'] = lapply(train['Cabin'],as.character)
 train['deck'] = sapply(train[['Cabin']], substr, 1,1)
 
 # get name prefix
-train['Name'] = as.character(train[['Name']])
-train['prefix'] = regmatches(train[['Name']], regexpr("[[:alpha:]]+[.]", train[['Name']]))
-unique(train$prefix)
-train$prefix[train$prefix == "Lady."] = "Miss."
-train$prefix[train$prefix == "Mlle."] = "Miss."
-train$prefix[train$prefix == "Countess."] = "Miss." #should be Mrs.
-train$prefix[train$prefix == "Ms."] = "Miss."
-train$prefix[train$prefix == "Mme."] = "Mrs."
-
-train$prefix[train$prefix == "Master."] = "Mr."
-train$prefix[train$prefix == "Don."] = "Mr."
-train$prefix[train$prefix == "Rev."] = "Mr."
-train$prefix[train$prefix == "Col."] = "Mr."
-train$prefix[train$prefix == "Capt."] = "Mr."
-train$prefix[train$prefix == "Jonkheer."] = "Mr."
-train$prefix[train$prefix == "Sir."] = "Mr."
-train$prefix[train$prefix == "Major."] = "Mr."
+get_prefix <- function(data) {
+  data['Name'] = as.character(data[['Name']])
+  data['prefix'] = regmatches(data[['Name']], regexpr("[[:alpha:]]+[.]", data[['Name']]))
+  unique(data$prefix)
+  data$prefix[data$prefix == "Lady."] = "Miss."
+  data$prefix[data$prefix == "Mlle."] = "Miss."
+  data$prefix[data$prefix == "Countess."] = "Miss." #should be Mrs.
+  data$prefix[data$prefix == "Ms."] = "Miss."
+  data$prefix[data$prefix == "Mme."] = "Mrs."
+  data$prefix[data$prefix == "Dona."] = "Mrs."
+  
+  data$prefix[data$prefix == "Master."] = "Mr."
+  data$prefix[data$prefix == "Don."] = "Mr."
+  data$prefix[data$prefix == "Rev."] = "Mr."
+  data$prefix[data$prefix == "Col."] = "Mr."
+  data$prefix[data$prefix == "Capt."] = "Mr."
+  data$prefix[data$prefix == "Jonkheer."] = "Mr."
+  data$prefix[data$prefix == "Sir."] = "Mr."
+  data$prefix[data$prefix == "Major."] = "Mr."
+  return(data)
+}
+train = get_prefix(train)
+test = get_prefix(test)
 
 by_prefix = ggplot(data=train,aes(x=prefix,fill=as.factor(Survived))) + geom_bar(position="fill")
 # those with prefix Mr. are less likely to survive compared to those with prefixes Miss. or Mrs.
@@ -91,7 +127,7 @@ for (i in factor_test) {
 }
 
 ### fit and predict model
-to_test = c("Pclass","Sex","SibSp","Parch","Embarked","Age")
+to_test = c("Pclass","Sex","SibSp","Parch","Embarked","Age","prefix")
 new_train = train[,c("Survived",to_test)]
 
 # basic glm model
@@ -109,7 +145,6 @@ train_set <- new_train[sample,]
 val_set <- new_train[-sample,]
 
 # get accuracy on validation set
-# gives .77990 kaggle score 
 # predict model on test set - gives .82123 score w/ age buckets
 predictions = predict(base_model, val_set)
 fitted.results <- ifelse(predictions > 0.5,1,0)
